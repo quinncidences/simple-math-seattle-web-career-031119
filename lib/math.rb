65979e3ca9301bf3377a5f879cdd397ce97e4938
Quinn Cox <qrc.inpa@gmail.com>
2019-02-24 04:17:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,11 +15,13 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
+  num1%num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 + (( num2 * num3 ) / num4 )
 end
